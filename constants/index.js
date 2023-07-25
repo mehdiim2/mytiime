a0f961ee472ae5202fee3cd1e5fba0d564bebb21
mehdiim2 <140565458+mehdiim2@users.noreply.github.com>
2023-07-25 16:46:50 -0700

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x93b0da1bcb4010a9e5ef3adeaad36fdeae2c984e";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
